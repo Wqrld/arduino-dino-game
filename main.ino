3e3f8755a2e933bc3c3eb7b5ebde08af61047481
Wqrld <wereld03@gmail.com>
2020-11-12 21:13:19 +0100

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,13 +1,13 @@
 #include <Servo.h>
-int jumpActionArray[200] = {};//werkt tot 200 jumps
+long jumpActionArray[200] = {};//werkt tot 200 jumps
 int jumpActionArrayLength = 0;
 int currentJumpActionIndex = 0;
 int currentMoveBackActionIndex = 0;
-int moveBackArray[200] = {}; //tot 200 keer weer terug
+long moveBackArray[200] = {}; //tot 200 keer weer terug
 int moveBackActionLength = 0;
 Servo serv;
 int pos = 0;
-float dely = 100;
+float dely = 50;
 int lastLDR = 0;
 int LDR = 0;
 int nextmove = 0;
@@ -24,36 +24,39 @@ void setup() {
 void loop() {
 
   LDR = analogRead(0); //lees LDR waard
-  Serial.println("LDR value: " + LDR);
+  //Serial.println("LDR value: " + LDR);
 
   if (wasLastLoopAction == false && LDR < lastLDR - 5) {//anti bounce
     wasLastLoopAction = true;
     jumpActionArray[jumpActionArrayLength] = millis() + dely; // opdracht queuen
     jumpActionArrayLength++;
-    dely = dely - 1.15; // delay telkens verminderen
-    Serial.println("jump");
+    dely = dely - 1.25; // delay telkens verminderen
+    Serial.println("queue jump");
   } else {
     wasLastLoopAction = false;
   }
   lastLDR = LDR;
-
-
+  Serial.println(currentJumpActionIndex);
+  Serial.println(jumpActionArray[currentJumpActionIndex]);
   if (jumpActionArray[currentJumpActionIndex] != 0 && millis() > jumpActionArray[currentJumpActionIndex]) { // als huidige instructie tijd op de stack geweest is (jump)
-    Serial.println("runjump");
+    Serial.println("run jump");
     serv.write(15); //spatie in
     currentJumpActionIndex++; //de volgende actie klaarzetten
 
-    moveBackArray[moveBackActionLength] = millis() + 1000; //queue move back
+    moveBackArray[moveBackActionLength] = millis() + 200; //queue move back
     moveBackActionLength++;
   }
-  Serial.println(moveBackArray[currentMoveBackActionIndex] + " < next current > " + millis());
+
+  //Serial.println(moveBackArray[currentMoveBackActionIndex] + " < next current > " + millis());
+
   if (moveBackArray[currentMoveBackActionIndex] != 0 && millis() > moveBackArray[currentMoveBackActionIndex]) { //als huidige instructie tijd op stack geweestr is (moveback)
-    Serial.println("moveback");
+    Serial.println("move back");
     currentMoveBackActionIndex++; //wacht op volgende move back
     serv.write(3); //spatie uit
   }
 
   delay(100);//anders is LDR te specifiek, hier nog wat op vinden?
+
 
 
 }
